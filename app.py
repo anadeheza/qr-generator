af3c703d810ff97617600fce6dd5831fa7f14ec1
Ana <adehezaflaherty@gmail.com>
2026-05-19 18:09:41 -0300

    fixed return and deleted import

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask, render_template, request
-import requests
 
 app = Flask(__name__)
 
 def existe_url(url):
     if not url or not url.startswith(("http://", "https://")):
         return False
+    return True 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    qr_text = None
-
     if request.method == "POST":
         qr_text = request.form.get("user_input")
-
-    if(existe_url(qr_text)):
-        return render_template("index.html", qr_data = qr_text)
-    else:
-        return render_template("index.html", error="La URL no es válida.")
+        if(existe_url(qr_text)):
+            return render_template("index.html", qr_data = qr_text)
+        else:
+            return render_template("index.html", error="La URL no es válida.")
     
     return render_template("index.html", qr_data=None)
 
